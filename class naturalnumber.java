c59146c9779370e1c04debcb53e778b91dce65cf
sahil <135645401+sahilsharmas@users.noreply.github.com>
2023-06-13 19:53:30 +0530

    naturalnumber

---
+++ class naturalnumber.java
@@ -0,0 +1,15 @@
+// Write a program to calculate the sum of first 10 natural number.
+class naturalnumber
+{
+	public static void main ( String args[])
+		{
+				int i,j;
+ 					j=0;
+
+			{
+					for (i=0;i<=10;i++)
+					j=j+i;	
+		
+}System.out.print("natural number"+j);
+}	
+}
